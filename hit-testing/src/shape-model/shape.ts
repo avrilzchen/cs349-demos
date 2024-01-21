37d93514e39a538cb9cea041fd4d1b2bb3108ab2
Daniel Vogel <dvogel@uwaterloo.ca>
2024-01-21 16:54:23 -0500

    drawing demos

---
+++ hit-testing/src/shape-model/shape.ts
@@ -0,0 +1,17 @@
+export abstract class Shape {
+  fill: string = "grey";
+  stroke: string = "black";
+  strokeWidth = 1;
+
+  get isFilled() {
+    return this.fill != "";
+  }
+
+  get isStroked() {
+    return this.stroke != "" && this.strokeWidth > 0;
+  }
+
+  abstract draw(gc: CanvasRenderingContext2D): void;
+
+  abstract hitTest(mx: number, my: number): boolean;
+}
